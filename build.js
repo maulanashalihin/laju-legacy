bb78125a73a2658d6f43a65bdf89e20f65276a44
Maulana Shalihin <maulanashalihin@Maulanas-MacBook-Air.local>
2023-06-05 11:26:17 +0800

    change naming

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ const svelteInertiaPlugin = require("esbuild-svelte-inertia")
 const fs = require('fs');
 const path = require('path');
 
-const outfile = `/js/app-${makeid(10)}.js`;
+const outfile = `/js/app-${(Math.random() + 1).toString(36).substring(7)}.js`;
 
 
 
@@ -23,18 +23,6 @@ esbuild
 
 
 
-
-function makeid(length) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
-}
 
  
 
