3674225f387906a2ee3c797bfa0e74ecac61a0d5
maulana <maulana@drip.id>
2023-08-22 11:55:06 +0800

    change db

--- knexfile.ts
+++ knexfile.ts
@@ -5,43 +5,13 @@ import knex from "./app/services/DB";
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: "sqlite3",
+    client: "better-sqlite3",
     connection: {
       filename: "./dev.sqlite3"
     }
   },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  }
+ 
+ 
 
 };
 
