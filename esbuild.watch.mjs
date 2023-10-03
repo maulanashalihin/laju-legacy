807078cdfe385528d4943914dfd2f4246ff90ab2
maulana <maulana@drip.id>
2023-10-03 11:30:32 +0800

    error handling when watch build

--- esbuild.watch.mjs
+++ esbuild.watch.mjs
@@ -12,76 +12,78 @@ let hash = {}
 
 const port = 8001;
 
- 
-import {writeFileSync,readFileSync} from 'fs'; 
- 
+
+import { writeFileSync, readFileSync } from 'fs';
+
 
 
 async function Build(init) {
 
 
+  try {
 
-  const start = Date.now();
+    const start = Date.now();
 
-  
+    let result = await esbuild.build({
+      entryPoints: ["./resources/js/app.js"],
+      mainFields: ["svelte", "browser", "module", "main"],
+      conditions: ["svelte", "browser"],
+      bundle: true,
+      minify: true,
+      write: false,
+      plugins: [sveltePlugin(), svelteInertiaPlugin()],
+      outdir: 'public',
+    })
 
-  let result = await esbuild.build({
-    entryPoints: ["./resources/js/app.js"],
-    mainFields: ["svelte", "browser", "module", "main"],
-    conditions: ["svelte", "browser"],
-    bundle: true,
-    minify: true,
-    write: false,
-    plugins: [sveltePlugin(), svelteInertiaPlugin()],
-    outdir: 'public',
-  })
+    const end = Date.now();
 
-  const end = Date.now();
+    console.log(`compile done in ${end - start}ms`)
 
-  console.log(`compile done in ${end - start}ms`)
+    const out = result.outputFiles;
 
-  const out = result.outputFiles;
-
-  let manifest = `{
+    let manifest = `{
   "style.css" : "http://localhost:${port}/style.css",`;
 
-  let count = 0;
+    let count = 0;
 
-  let push_change = false;
+    let push_change = false;
 
-  for await (const file of out) {
-    count++;
-    const filename = file.path.split("/").pop();
-    files[filename] = file.text;
-    if(hash[filename] != file.hash)
-    {
-      push_change = true;
-      hash[filename] = file.hash;
+    for await (const file of out) {
+      count++;
+      const filename = file.path.split("/").pop();
+      files[filename] = file.text;
+      if (hash[filename] != file.hash) {
+        push_change = true;
+        hash[filename] = file.hash;
+      }
+      manifest += `"${filename}": "http://localhost:${port}/${filename}"${count < out.length ? "," : ""}`
+
     }
-    manifest += `"${filename}": "http://localhost:${port}/${filename}"${count < out.length  ? "," : ""}`
-   
+
+    if (push_change && !init) {
+      console.log("compile done, pushing change...")
+      clients.forEach((client) => {
+
+        const send = `data: reload\n\n`;
+
+        client.response.write(send);
+      })
+    }
+
+
+    manifest += `}`
+
+    if (init) {
+      writeFileSync("./public/manifest.json", manifest)
+    }
+
+  } catch (error) {
+
   }
 
-  if(push_change && !init)
-  {
-    console.log("compile done, pushing change...")
-    clients.forEach((client) => {
-   
-      const send = `data: reload\n\n`;
-    
-      client.response.write(send);
-    })
-  }
-  
 
-  manifest+=`}`
- 
-  if(init)
-  {
-    writeFileSync( "./public/manifest.json", manifest)
-  }
-    
-  
+
+
 }
 
 
@@ -90,7 +92,7 @@ import chokidar from "chokidar"
 var watcher = chokidar.watch('resources/js', { ignored: /^\./, persistent: true });
 
 watcher
-  .on('ready', ()=>{
+  .on('ready', () => {
     console.log('Initial scan complete. Ready for changes');
     Build(true)
   })
@@ -104,7 +106,7 @@ import HyperExpress from 'hyper-express';
 const webserver = new HyperExpress.Server();
 
 
- 
+
 import cors from "cors"
 
 webserver.use(cors())
@@ -144,20 +146,19 @@ webserver.get('*', (request, response) => {
   const ext = path.split(".")[1];
 
   try {
- 
+
 
     let file;
 
-    if(files[path])
-    {
+    if (files[path]) {
       file = files[path]
-    }else{ 
-      file = readFileSync("public/"+path);
+    } else {
+      file = readFileSync("public/" + path);
     }
 
 
     if (path.includes('.js')) {
-      
+
       file += `
         
         var evtSource = new EventSource('http://localhost:${port}/subscribe');
