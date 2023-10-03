e4b11cc8b72c1db8ccccd56f0f48bc4f6b22693b
maulana <maulana@drip.id>
2023-10-03 16:19:01 +0800

    change readme

---
+++ app/controllers/UserController.ts
@@ -0,0 +1,31 @@
+
+import { Response, Request } from "../../type"; 
+import DB from "../services/DB";
+
+class Controller {
+    
+  public async index (request : Request,response : Response) { 
+  }
+
+  public async create (request : Request, response : Response) {
+  }
+
+  public async store (request : Request, response : Response) {
+  }
+
+  public async show (request : Request, response : Response) {
+  }
+
+  public async edit (request : Request, response : Response) {
+  }
+
+  public async update (request : Request, response : Response) {
+  }
+
+  public async destroy (request : Request, response : Response) {
+  }
+
+}
+
+export default new Controller()
+  
